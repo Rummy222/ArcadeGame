838ed7b47fd3d2d18f69bac8a88268051002401c
David Rohm <dr9557@gmail.com>
2017-12-19 10:16:10 -0500

    firstUpdate

--- js/app.js
+++ js/app.js
@@ -1,38 +1,56 @@
-// Enemies our player must avoid
-var Enemy = function() {
+// 1.)    Enemies our player must avoid
+var Enemy = function(x,y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+       this.x = x;
+       this.y = y;
+       this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    // dbr - movement: var loc = loc++?
+    // dbr - needs a handleinput method
+    // dbr - needs a addEventListener method
+    // dbr - needs a drawImage method
+    // dbr - needs a update method
 };
 
-// Update the enemy's position, required method for game
+// 2.)    Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+//loc = movement that you gett from the handler * dt
 };
-
-// Draw the enemy on the screen, required method for game
+// 3.)    Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
+// 4.)    Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var player = function(x,y,speed){
+      this.x = x;
+      this.y = y;
+      this.speed = speed;
+      this.sprite = 'images/char-boy.png';
+ };
+
+// 5.)    Place all enemy objects in an array called allEnemies
+// 6.)    Place the player object in a variable called player
 
 
+var allEnemies = [];
+  allEnemies = [new Enemy(1,2,3), new Enemy(1,2,3)];
 
-// This listens for key presses and sends the keys to your
+player = new player(20, 38, 50);
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//  This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
